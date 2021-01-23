fada43c3e883a40e3561192c42ea257d2d9a07b3
Romulo Santos <romulosantos88@hotmail.com>
2021-01-23 12:36:06 -0300

    event and methods

--- eventos-e-metodos/App.vue
+++ eventos-e-metodos/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
     <h1>Minha lista de tarefas:</h1>
-    <button @click="() => showList = !showList">
+    <button @click="handleShowHideList">
       Ver a lista
     </button>
     <br>
 
-    <input type="text v-focus">
+    <input 
+      type="text"
+      @keyup.enter="addTask"
+      v-focus 
+      v-model="currentTask">
 
     <ul v-if="showList">
       <li v-for="(task, index) in tasks"
+      @dblclick="complete(task)"
       :key="`${task}-${index}`"
+      class="task-item"
+      :class="{
+        'line_through': task.isDone
+        }"
       >
         {{ task.name }}
+        <button
+        @click="remove(task)"
+        >&times;</button>
       </li>
     </ul>
     <p v-else> Lista de tarefas escondidas</p>
@@ -31,14 +43,44 @@ export default {
     focus
   },
  data: () => ({
+   currentTask: '',
    showList: false,
    tasks: [
      {name: 'Fazer o curso', isDone: false}
    ]
- })
+ }),
+ methods: {
+   complete (task) {
+    this.tasks = this.tasks.map(t => { 
+      if (t.name === task.name) {
+        return { ...t, isDone: !t.isDone }
+      }
+      return {...t}
+    })
+   },
+   handleShowHideList () {
+     this.showList = !this.showList
+   },
+   remove (task) {
+     this.tasks = this.tasks.filter(t => t.name !== task.name)
+   },
+   addTask () {
+     this.tasks.push({ 
+       name: this.currentTask,
+       isDone: false
+     })
+     this.currentTask = ''
+   },
+ }
 }
 </script>
 
-<style>
+<style scoped>
+.line_through {
+    text-decoration: line-through;
+  }
+.task-item {
+cursor: pointer;
+}
 
 </style>
